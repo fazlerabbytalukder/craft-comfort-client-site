008dabc6f0952b5f95d77dfbe7bd457e00e31b15
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-09-28 10:41:26 -0700

    firebase setup change

--- src/pages/Login/Firebase/firebase.config.js
+++ src/pages/Login/Firebase/firebase.config.js
@@ -1,9 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAZbj5d3PfGA-x7xSNlIcE-o8tLwDTbTI0",
-  authDomain: "tourist-travel-bd.firebaseapp.com",
-  projectId: "tourist-travel-bd",
-  storageBucket: "tourist-travel-bd.appspot.com",
-  messagingSenderId: "500739694862",
-  appId: "1:500739694862:web:fe3447f30d9a6c5b18b75f"
+  apiKey: "AIzaSyAD1Bv8K4Fnny3W6v_Inks8DsbBrvyzd5M",
+  authDomain: "craft-comfort.firebaseapp.com",
+  projectId: "craft-comfort",
+  storageBucket: "craft-comfort.appspot.com",
+  messagingSenderId: "813667713763",
+  appId: "1:813667713763:web:3d23c94055adbb2939bbd6",
+  measurementId: "G-94WRWC23GR"
 };
 export default firebaseConfig;
