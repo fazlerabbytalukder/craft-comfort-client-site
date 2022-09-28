3d8335e460fca73c2aad64b8765185c05c960a7d
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-09-28 02:04:06 -0700

    main nav

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Register from './pages/Login/Register/Register';
 
 function App() {
   return (
-    <div>
+    <div className='transition duration-500 font-poppins'>
       <AuthProvider>
         <BrowserRouter>
           <Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        roboto:"'Roboto', sans-serif"
+      },
+      colors: {
+        'primary':'#0C1222',
+        'secondary':'#748098',
+        'ternary': '#E2E8F0',
+        'main':'#38BDF8',
+      }
+    },
   },
   plugins: [],
 }
