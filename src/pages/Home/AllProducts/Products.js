c5d96feebbf69f1ccc517470a04599d10f36d739
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-10-01 20:13:58 -0700

    loading added in product component

--- src/pages/Home/AllProducts/Products.js
+++ src/pages/Home/AllProducts/Products.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import Heading from '../AllComponents/Heading';
 import Product from './Product';
+import { ImSpinner10 } from "react-icons/im";
 
 
-const Products = ({ title, description, reviewNumer, value }) => {
+const Products = () => {
     const [furnitures, setFurnitures] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
         fetch('http://localhost:5000/furnitures')
             .then(res => res.json())
             .then(data => {
-                setFurnitures(data)
+                setFurnitures(data);
+                setIsLoading(false);
             });
     }, [])
 
@@ -18,7 +22,11 @@ const Products = ({ title, description, reviewNumer, value }) => {
 
     return (
         <div className='bg-[#F8F9FC] dark:bg-[#0F172A] pb-8'>
+            {/* heading components  */}
             <Heading title="Our Products" description="This is our products and here you find your desire furniture." />
+            {/* loading added if item not shown */}
+            {isLoading && <div className='flex justify-center items-center'><ImSpinner10 className='animate-spin text-5xl' /></div>}
+            {/* product container  */}
             <section className="text-gray-600 body-font">
                 <div className="container px-5 mx-auto">
                     <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-4">
@@ -28,6 +36,7 @@ const Products = ({ title, description, reviewNumer, value }) => {
                     </div>
                 </div>
             </section>
+            {/* product container end  */}
         </div>
     );
 };
