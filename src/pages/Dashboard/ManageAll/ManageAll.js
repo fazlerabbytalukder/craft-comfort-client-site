edb7dc51d0b7138f34ee91083912721d919edfce
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-10-09 09:30:55 -0700

    delete order

--- src/pages/Dashboard/ManageAll/ManageAll.js
+++ src/pages/Dashboard/ManageAll/ManageAll.js
@@ -37,6 +37,28 @@ const ManageAll = () => {
     }
 
 
+    //handle delete
+    const handleDelete = (id) => {
+        setApproveId(id);
+        const proceed = window.confirm('Are you sure, you want to delete?');
+        if (proceed) {
+            const url = `http://localhost:5000/orders/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+                        const remainingOrder = orderData.filter(user => user._id !== id);
+                        setOrderData(remainingOrder);
+                        // window.reload();
+                    }
+                });
+        }
+    }
+
+
     return (
         <div className='overflow-auto'>
             <table className='shadow-2xl border-2 border-primary w-full'>
@@ -59,7 +81,7 @@ const ManageAll = () => {
                             <td className='py-3 px-6 whitespace-nowrap'>{row.furnitureName}</td>
                             <td className='py-3 px-6 whitespace-nowrap'>{row.status === "pending" ? <span className='bg-red-100 px-3 py-1 rounded-full'>{row.status}</span> : <span className='bg-lime-100 px-3 py-1 rounded-full'>{row.status}</span>}</td>
                             <td className="py-3 px-6 whitespace-nowrap"><button className={`bg-primary text-white px-3 py-1 rounded ${row.status === "Shipped" && "opacity-50 cursor-not-allowed"}`} onClick={() => handleUpdate(row._id)}>{row.status === "pending" ? <span>Approve</span> : <span>Shipped</span>}</button></td>
-                            <td className='py-3 px-6 whitespace-nowrap'><button className='bg-primary text-white px-3 py-1 rounded'>Delete</button></td>
+                            <td className='py-3 px-6 whitespace-nowrap'><button className='bg-primary text-white px-3 py-1 rounded' onClick={() => handleDelete(row._id)}>Delete</button></td>
                         </tr>
                     ))}
                 </tbody>
