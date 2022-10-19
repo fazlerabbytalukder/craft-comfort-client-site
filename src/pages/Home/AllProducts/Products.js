49fcac33694ed908fdd883ed1bcf254602d6a000
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-10-19 09:33:28 -0700

    some comment remove and added

--- src/pages/Home/AllProducts/Products.js
+++ src/pages/Home/AllProducts/Products.js
@@ -17,6 +17,8 @@ const Products = () => {
     const [size, setSize] = useState(8);
     const { quantity } = useContext(DataProvider);
 
+
+    // furniture count fetch 
     useEffect(() => {
         getProductsCount();
     }, [])
@@ -31,7 +33,7 @@ const Products = () => {
         }
     }
 
-
+    // furniture fetch 
     useEffect(() => {
         getProducts();
     }, [page, size])
@@ -49,6 +51,7 @@ const Products = () => {
     }
 
 
+    // handle add to cart 
     const handleAddToCart = (selectedFurnitures) => {
         let newCart = [];
         const exist = cart.find(furniture => furniture._id === selectedFurnitures.id);
@@ -77,11 +80,13 @@ const Products = () => {
                     {
                         isLoading && <div className='flex justify-center items-center'><ImSpinner10 className='animate-spin text-5xl' /></div>
                     }
+                    {/* products container  */}
                     <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-4">
                         {
                             furnitures.map(furniture => <Product key={furniture._id} furniture={furniture} handleAddToCart={handleAddToCart} />)
                         }
                     </div>
+                    {/* pagination section  */}
                     <div className='flex items-center justify-end'>
                         <div className='space-x-2 mt-6'>
                             {
@@ -100,49 +105,6 @@ const Products = () => {
                     </div>
                 </div>
             </section>
-
-            {/* 
-            <div className='bg-[#F8F9FC] dark:bg-[#0F172A] pb-8'>
-                <Heading title="Our Products" description="This is our products and here you find your desire furniture." />
-                {isLoading && <div className='flex justify-center items-center'><ImSpinner10 className='animate-spin text-5xl' /></div>}
-
-
-                <div className='container px-5 mx-auto lg:flex lg:justify-between lg:items-center mb-8'>
-                    <Search searchName={searchName} />
-                    <Filter filterContinent={filterContinent} />
-                </div>
-
-
-
-                <section className="text-gray-600 body-font">
-                    <div className="container px-5 mx-auto">
-                        <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-4">
-                            {
-                                displayFurniture.map(furniture => <Product key={furniture._id} furniture={furniture} />)
-                            }
-                        </div>
-
-                        <div className='flex items-center justify-end'>
-                            <div className='space-x-2 mt-6'>
-                                {
-                                    [...Array(pageCount).keys()].map(number => <button
-                                        onClick={() => setPage(number)}
-                                        className={`border border-[#FD3D57] px-4 py-1 font-bold dark:text-white ${page === number ? "bg-[#FD3D57]" : ""}`}
-                                    >{number}</button>)
-                                }
-                                <select className='border border-[#FD3D57] px-4 py-1 font-bold dark:text-white' onChange={e => setSize(e.target.value)}>
-                                    <option value="8" selected>8</option>
-                                    <option value="10">10</option>
-                                    <option value="12">12</option>
-                                    <option value="14">14</option>
-                                </select>
-                            </div>
-                        </div>
-
-                    </div>
-                </section>
-
-            </div> */}
         </div>
     );
 };
