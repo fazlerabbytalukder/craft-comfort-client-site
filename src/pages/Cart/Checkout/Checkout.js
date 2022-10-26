e2cb7f08fb8e0626e9bdf63d0bd1148333c75e6c
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-10-26 05:27:34 -0700

    checkout form error fixed

--- src/pages/Cart/Checkout/Checkout.js
+++ src/pages/Cart/Checkout/Checkout.js
@@ -6,6 +6,13 @@ import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
 import { BsFillCheckCircleFill } from 'react-icons/bs';
 
+const initialFormData = {
+    streetAddress: "",
+    townCity: "",
+    zipCode: "",
+    phone: "",
+}
+
 const Checkout = () => {
     const { user } = useAuth();
     const [cart] = useCart();
@@ -19,6 +26,7 @@ const Checkout = () => {
     const email = user.email;
     const initialInfo = { yourName, email };
     const [orderInfo, setOrderInfo] = useState(initialInfo);
+    const [formData, setFormData] = useState(initialFormData);
 
     useEffect(() => {
         let q = 0
@@ -34,19 +42,12 @@ const Checkout = () => {
         setGrandTotal(t + tax)
     }, [cart])
 
-    const handleOnBlur = (e) => {
-        const field = e.target.name;
-        const value = e.target.value;
-
-        const newInfo = { ...orderInfo };
-        newInfo[field] = value;
-        setOrderInfo(newInfo);
-    };
-
 
     const handleOrderSubmit = (e) => {
+        e.preventDefault();
         const productOrder = {
             ...orderInfo,
+            ...formData,
             productInfo: cart,
             total: total,
             grandTotal: grandTotal,
@@ -67,10 +68,9 @@ const Checkout = () => {
             .then((data) => {
                 if (data.insertedId) {
                     setorderSuccess(true);
+                    setFormData(initialFormData);
                 }
             });
-
-        e.preventDefault();
     };
 
     return (
@@ -79,23 +79,71 @@ const Checkout = () => {
             <section className='container px-5 mx-auto'>
                 <div className='flex w-full flex-col gap-8 sm:flex-row sm:items-center items-start mx-auto'>
                     <div className='flex-grow w-full'>
-                    <h1 className='text-center text-4xl font-bold mt-3 dark:text-white'>Checkout Form</h1>
+                        <h1 className='text-center text-4xl font-bold mt-3 dark:text-white'>Checkout Form</h1>
                         <form onSubmit={handleOrderSubmit}>
                             <div className="relative mb-4">
-                                <label for="street-address" className="leading-7 text-sm text-gray-600 dark:text-white">Street Address</label>
-                                <input type="text" name='street-address' placeholder='treet Address' required autocomplete="off" className="w-full bg-formBg dark:bg-darkFormBg rounded border border-gray-300 dark:border-navDark focus:border-indigo-500 focus:ring-2 text-base outline-none text-gray-700 dark:text-ternary py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleOnBlur} />
+                                <label
+                                    for="street-address"
+                                    className="leading-7 text-sm text-gray-600 dark:text-white"
+                                >Street Address
+                                </label>
+                                <input
+                                    type="text"
+                                    name='street-address'
+                                    placeholder='treet Address'
+                                    required
+                                    autocomplete="off"
+                                    className="w-full bg-formBg dark:bg-darkFormBg rounded border border-gray-300 dark:border-navDark focus:border-indigo-500 focus:ring-2 text-base outline-none text-gray-700 dark:text-ternary py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    value={formData.streetAddress}
+                                    onChange={e => setFormData({ ...formData, streetAddress: e.target.value })} />
                             </div>
                             <div className="relative mb-4">
-                                <label for="town-city" className="leading-7 text-sm text-gray-600 dark:text-white">Town/City</label>
-                                <input type="text" name='town-city' placeholder='Town/City' required autocomplete="off" className="w-full bg-formBg dark:bg-darkFormBg rounded border border-gray-300 dark:border-navDark focus:border-indigo-500 focus:ring-2 text-base outline-none text-gray-700 dark:text-ternary py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleOnBlur} />
+                                <label
+                                    for="town-city"
+                                    className="leading-7 text-sm text-gray-600 dark:text-white">
+                                    Town/City
+                                </label>
+                                <input
+                                    type="text"
+                                    name='town-city'
+                                    placeholder='Town/City'
+                                    required
+                                    autocomplete="off"
+                                    className="w-full bg-formBg dark:bg-darkFormBg rounded border border-gray-300 dark:border-navDark focus:border-indigo-500 focus:ring-2 text-base outline-none text-gray-700 dark:text-ternary py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    value={formData.townCity}
+                                    onChange={e => setFormData({ ...formData, townCity: e.target.value })} />
                             </div>
                             <div className="relative mb-4">
-                                <label for="zip-code" className="leading-7 text-sm text-gray-600 dark:text-white">Zip Code</label>
-                                <input type="text" name='zip-code' placeholder='Zip Code' required autocomplete="off" className="w-full bg-formBg dark:bg-darkFormBg rounded border border-gray-300 dark:border-navDark focus:border-indigo-500 focus:ring-2 text-base outline-none text-gray-700 dark:text-ternary py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleOnBlur} />
+                                <label
+                                    for="zip-code"
+                                    className="leading-7 text-sm text-gray-600 dark:text-white">
+                                    Zip Code
+                                </label>
+                                <input
+                                    type="text"
+                                    name='zip-code'
+                                    placeholder='Zip Code'
+                                    required
+                                    autocomplete="off"
+                                    className="w-full bg-formBg dark:bg-darkFormBg rounded border border-gray-300 dark:border-navDark focus:border-indigo-500 focus:ring-2 text-base outline-none text-gray-700 dark:text-ternary py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    value={formData.zipCode}
+                                    onChange={e => setFormData({ ...formData, zipCode: e.target.value })} />
                             </div>
                             <div className="relative mb-4">
-                                <label for="phone" className="leading-7 text-sm text-gray-600 dark:text-white">Your Number</label>
-                                <input type="text" name='phone' placeholder='Your Number' required autocomplete="off" className="w-full bg-formBg dark:bg-darkFormBg rounded border border-gray-300 dark:border-navDark focus:border-indigo-500 focus:ring-2 text-base outline-none text-gray-700 dark:text-ternary py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleOnBlur} />
+                                <label
+                                    for="phone"
+                                    className="leading-7 text-sm text-gray-600 dark:text-white">
+                                    Your Number
+                                </label>
+                                <input
+                                    type="text"
+                                    name='phone'
+                                    placeholder='Your Number'
+                                    required
+                                    autocomplete="off"
+                                    className="w-full bg-formBg dark:bg-darkFormBg rounded border border-gray-300 dark:border-navDark focus:border-indigo-500 focus:ring-2 text-base outline-none text-gray-700 dark:text-ternary py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    value={formData.phone}
+                                    onChange={e => setFormData({ ...formData, phone: e.target.value })} />
                             </div>
                             {
                                 orderSuccess && (
